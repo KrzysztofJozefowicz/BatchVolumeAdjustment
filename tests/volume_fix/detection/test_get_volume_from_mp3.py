fb549874b79bac7420a921e1d66a2c9f29f9c389
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2020-03-18 22:34:18 +0100

    changed monkeypatch to mocker

--- tests/volume_fix/detection/test_get_volume_from_mp3.py
+++ tests/volume_fix/detection/test_get_volume_from_mp3.py
@@ -6,13 +6,14 @@ import mp3_eq_vol.src.volume_fix.detection as detection
 import pytest
 
 @pytest.fixture()
-def mocked_internals(monkeypatch):
+def mocked_internals(mocker):
     def return_output_from_subprocess_mocked(ffmpeg_args, stderr):
         return "".encode("utf-8")
     def get_details_from_ffmpeg_output_mocked(input):
         return {"mean_volume":0 , "max_volume":10}
-    monkeypatch.setattr(detection.subprocess, 'check_output', return_output_from_subprocess_mocked)
-    monkeypatch.setattr(detection.volume_detection,'get_details_from_ffmpeg_output',get_details_from_ffmpeg_output_mocked)
+
+    mocker.patch('subprocess.check_output', side_effect=return_output_from_subprocess_mocked)
+    mocker.patch('mp3_eq_vol.src.volume_fix.detection.volume_detection.get_details_from_ffmpeg_output',side_effect=get_details_from_ffmpeg_output_mocked)
 
 
 def test_get_volume_from_mp3_check_keys(mocked_internals):
