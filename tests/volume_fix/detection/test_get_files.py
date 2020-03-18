73e5fb92aee26d377f81b2efb572869503886707
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2020-03-18 21:39:17 +0100

    made mock common across tests for funct

--- tests/volume_fix/detection/test_get_files.py
+++ tests/volume_fix/detection/test_get_files.py
@@ -14,38 +14,36 @@ import mp3_eq_vol.src.volume_fix.detection as detection
 
 import pytest
 
+
+@pytest.fixture()
+def mocked_internals(monkeypatch):
+
+    def is_file_mocked(file):
+        if file[-3:]=='mp3' or file[-3:]=='txt':
+            return True
+        else:
+            return False
+    monkeypatch.setattr(detection, 'isfile', is_file_mocked)
+
+
 @pytest.mark.parametrize("return_list_of_files,input_path,asertion",
                          [(['1.mp3','2.mp3','folder','3.txt'],'C:\\pycharm\\mp3_eq_vol\\origin\\',['C:\\pycharm\\mp3_eq_vol\\origin\\1.mp3','C:\\pycharm\\mp3_eq_vol\\origin\\2.mp3']),
                           (['1.mp3'],'C:\\pycharm\\mp3_eq_vol\\origin\\1.mp3' ,['C:\\pycharm\\mp3_eq_vol\\origin\\1.mp3']),
                           ])
-def test_get_files(monkeypatch,return_list_of_files,input_path,asertion):
+def test_get_files(monkeypatch,return_list_of_files,input_path,asertion,mocked_internals):
+
     def return_list_files_in_dir(path):
         return(return_list_of_files)
-
-    def is_file_mocked(file):
-        if file[-3:]=='mp3' or file[-3:]=='txt':
-            return True
-        else:
-            return False
-
     monkeypatch.setattr(detection, 'listdir', return_list_files_in_dir)
-    monkeypatch.setattr(detection, 'isfile', is_file_mocked)
-
     assert detection.volume_detection.get_files(input_path) == asertion
 
 @pytest.mark.parametrize("return_list_of_files,input_path",
                          [(['1.mp3'],'C:\\1.txt'),(['1.mp3'],'C:\folder')] )
-def test_get_files_with_non_valid_input(monkeypatch,return_list_of_files,input_path):
+def test_get_files_with_non_valid_input(monkeypatch,return_list_of_files,input_path,mocked_internals):
+
     def return_list_files_in_dir(path):
         return(return_list_of_files)
-
-    def is_file_mocked(file):
-        if file[-3:]=='mp3' or file[-3:]=='txt':
-            return True
-        else:
-            return False
-
     monkeypatch.setattr(detection, 'listdir', return_list_files_in_dir)
-    monkeypatch.setattr(detection, 'isfile', is_file_mocked)
+
     with pytest.raises(Exception):
         detection.volume_detection.get_files(input_path)
