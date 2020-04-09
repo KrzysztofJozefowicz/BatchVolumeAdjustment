059553d2ba84694f4953e8e8683f7076b5dba2ff
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2020-04-09 20:35:35 +0200

    cleanup

--- src/volume_fix/adjustment.py
+++ src/volume_fix/adjustment.py
@@ -8,14 +8,16 @@ from mp3_eq_vol.src.volume_fix.run_async import run_async
 class Adjustment():
 
     @classmethod
-    def set_volume_async(cls, song_list, offset_list, output_dir):
+    def set_volume(cls, song_list, offset_list, output_dir):
 
         set_volume_commands=[]
+        tasks_names=[]
         for song in song_list:
             filename, offset_dB, output_filepath = song, offset_list[song], os.path.join(output_dir, os.path.split(song)[-1])
             task_command=['ffmpeg','-y', '-i', filename, '-filter:a', "volume=" + str(offset_dB) + "dB",output_filepath]
             set_volume_commands.append(task_command)
-        run_async.run_concurent_async(set_volume_commands,[song for song in song_list])
+            tasks_names.append(song)
+        run_async.run_concurent_async(set_volume_commands,tasks_names)
 
 
 if __name__ == "__main__":
@@ -33,4 +35,4 @@ if __name__ == "__main__":
     #maximum_volume=song_list[song]["max_volume"]
     offset_list= volume_analysis.volume_analysis.find_volume_offset_based_on_max_max_volume(song_max_volume, song_list)
     #Adjustment.set_volume(song_list,offset_list,output_dir)
-    Adjustment.set_volume_async(song_list, offset_list, output_dir)
+    Adjustment.set_volume(song_list, offset_list, output_dir)
