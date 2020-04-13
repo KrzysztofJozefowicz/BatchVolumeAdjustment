691709386d15321e3924e1e6fb2376ece2a037ae
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2020-04-13 15:15:57 +0200

    cleanup

--- src/volume_fix/adjustment.py
+++ src/volume_fix/adjustment.py
@@ -25,14 +25,6 @@ if __name__ == "__main__":
     mypath = 'C:\pycharm\mp3_eq_vol\origin'
 
     song_list=volume_detection.volume_detection.analyse_volume_from_files(mypath)
-    #for song in song_list:
-    #    print (song,song_list[song]["mean_volume"],song_list[song]["max_volume"])
-    #print("max  mean volume")
-    #print(volume_analysis.volume_analysis.find_max_mean_volume(song_list))
-    #print("max max volume")
     song_max_volume= volume_analysis.volume_analysis.find_max_max_volume(song_list)
-    #print(song_max_volume)
-    #maximum_volume=song_list[song]["max_volume"]
     offset_list= volume_analysis.volume_analysis.find_volume_offset_based_on_max_max_volume(song_max_volume, song_list)
-    #Adjustment.set_volume(song_list,offset_list,output_dir)
     Adjustment.set_volume(song_list, offset_list, output_dir)
